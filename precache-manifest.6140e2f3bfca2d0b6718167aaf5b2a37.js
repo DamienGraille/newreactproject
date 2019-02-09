1b9fe10d5f9b61488aa943ec65df1b39877a0b69
Damien1989 <damiengraille@gmail.com>
2019-02-09 18:49:38 +0000

    Updates

---
+++ precache-manifest.6140e2f3bfca2d0b6718167aaf5b2a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newreactproject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a78da58a807a0f5de9a",
+    "url": "/newreactproject/static/js/runtime~main.1a78da58.js"
+  },
+  {
+    "revision": "9e3b8e0f9b35506e3b22",
+    "url": "/newreactproject/static/js/main.9e3b8e0f.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/newreactproject/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "9e3b8e0f9b35506e3b22",
+    "url": "/newreactproject/static/css/main.c4b7308e.chunk.css"
+  },
+  {
+    "revision": "61385c458de144dd3242000c9304fb60",
+    "url": "/newreactproject/index.html"
+  }
+];
